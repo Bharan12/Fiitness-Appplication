28b153f2970c7b04643b6040586429622453b6d1
Bharan12 <bharanidharan1231@gmail.com>
2024-10-08 13:57:32 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 const{MONGO_URL,PORT}=process.env
 const dbConnection=async()=>{ 
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017');
+        const conn = await mongoose.connect('mongodb+srv://Bharan12:Adhya21@cluster0.pi3ok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
